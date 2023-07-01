4437f2fba9069b71f785025a04e3590ab763697d
koi2000 <1754520660@qq.com>
2023-07-01 21:36:02 +0800

    完成猜数游戏

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,29 @@
-use std::io;
+use rand::Rng;
+use std::{io, cmp::Ordering};
 
 fn main() {
     println!("猜数！");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    print!("神秘数字是：{}", secret_number);
+    loop {
+        println!("猜测一个数");
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("无法读取行");
-    println!("你猜测的数为：{}", guess);
+        io::stdin().read_line(&mut guess).expect("无法读取行");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("你猜测的数为：{}", guess);
+        match guess.cmp(&secret_number) {  
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater   => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+    }
 }
