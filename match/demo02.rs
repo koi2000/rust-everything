3efda1ff0c0a0cfa90569710e98e488e9721750d
koi2000 <1754520660@qq.com>
2023-07-02 22:10:45 +0800

    新增match

---
+++ match/demo02.rs
@@ -0,0 +1,26 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {}
+
+
+fn main() {
+    let re = value_in_cents(Coin::Penny);
+    println!("{}", re);
+}
---
+++ match/demo05.rs
@@ -0,0 +1,16 @@
+fn test01() {
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {}", max),
+        _ => (),
+    }
+}
+
+fn test02() {
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+}
+
+fn main() {}
