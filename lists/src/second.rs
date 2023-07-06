bbde4fe7b30a39e84a10b67422bd475d9b98c72e
koi2000 <koi20000@163.com>
2023-07-06 12:58:22 +0800

    新增链表 version02

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -1,4 +1,4 @@
-use std::mem;
+// use std::mem;
 
 // pub struct List {
 //     head: Link,
@@ -47,6 +47,68 @@ use std::mem;
 
 pub struct List<T> {
     head: Link<T>,
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+// 这里无需生命周期，因为 List 没有使用生命周期的关联项
+impl<T> List<T> {
+    // 这里我们为 iter 声明一个生命周期'a ，此时&self需要至少和iter活得一样久
+    pub fn iter<'a>(&'a self) -> Iter<'a,T> {
+        Iter{next:self.head.as_deref()}
+    }
+}
+
+// 这里声明生命周期是因为下面的关联类型 Item 需要
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    // 这里无需更改，因为上面已经处理了.
+    // Self 依然是这么棒
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
+pub struct IterMut<'a,T>{
+    next:Option<&'a mut Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter_mut(&mut self) -> IterMut<'_,T>{
+        IterMut{next: self.head.as_deref_mut()}
+    }
+}
+
+impl<'a,T> Iterator for IterMut<'a,T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self)->Option<Self::Item>{
+        self.next.take().map(|node|{
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
+        })
+    }
 }
 
 type Link<T> = Option<Box<Node<T>>>;
@@ -139,15 +201,50 @@ mod test {
         list.push(1);
         list.push(2);
         list.push(3);
-        assert_eq!(list.peek(), Some(3));
+        assert_eq!(list.peek(), Some(&3));
         assert_eq!(list.peek_mut(), Some(&mut 3));
         // list.peek_mut().map(|&mut value| {
         //     value = 42
         // });
-        list.peek_mut().map(|value| {
-            *value = 42
-        });
+        list.peek_mut().map(|value| *value = 42);
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn test_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        let mut iter = list.iter();
+        assert_eq!(iter.next(),Some(&3));
+        assert_eq!(iter.next(),Some(&2));
+        assert_eq!(iter.next(),Some(&1));
+    }
+
+    #[test]
+    fn iter_mut(){
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(),Some(&mut 3));
+        assert_eq!(iter.next(),Some(&mut 2));
+        assert_eq!(iter.next(),Some(&mut 1));
     }
 }
